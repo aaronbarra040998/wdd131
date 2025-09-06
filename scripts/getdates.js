a91b99668aaadeb2e9a1fb1d87de10c6d7eb49c5
aaron040998 <aaron.a.barra1998@gmail.com>
2025-09-06 15:08:59 -0500

    comple_1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the copyright year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set the last modified date
+document.getElementById('lastModified').textContent = 'Last Modification: ' + document.lastModified;
